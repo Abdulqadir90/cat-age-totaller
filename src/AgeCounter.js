a8a20796b2a426f3e3f81a282e9a95db0c377c9b
Abdulqadir90 <33445223+Abdulqadir90@users.noreply.github.com>
2021-06-04 21:16:41 +0100

    completed

--- src/AgeCounter.js
+++ src/AgeCounter.js
@@ -3,19 +3,23 @@ import React, {useState} from 'react'
 export default function AgeCounter(props) {
     const [addYear, setAddYear] = useState(0)
 
-
-
     function ageCalculated(el) {
         setAddYear(addYear + el)
-        props.update(el)
+        props.improved(el)
 
     }
+
+    const updatedAge = addYear < 9 ? "young" : "old";
+
+
     return (
-        <div>
-            
+        <div className={`AgeCounter ${updatedAge}`}>
+
+
+          <h4>{props.cat}</h4>  
           <h3>{addYear}</h3>
-         <button onClick={() => ageCalculated}>adds year</button>   
-         <button onClick={() => ageCalculated()}>new life</button>   
+         <button onClick={() => ageCalculated(1)}>adds year</button>   
+         <button onClick={() => ageCalculated(-addYear)}>new life</button>   
         </div>
     )
 }
--- src/AgeTotaller.js
+++ src/AgeTotaller.js
@@ -1,20 +1,40 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import AgeCounter from "./AgeCounter";
 
 const AgeTotaller = () => {
-const [cats, setCats] = useState[("Tobi", "Dogba", "Catzo", "Whiskers")];
-const [newCat, setNewCat] = useState("");
-const [totalAge, setTotalAge] = useState(0);
+  const [cats, setCats] = useState(["Tobi", "Dogba", "Catzo", "Whiskers"]);
+  const [newCat, setNewCat] = useState("");
+  const [totalAge, setTotalAge] = useState(0);
 
+  function improved(el) {
+    setTotalAge(totalAge + el);
+  }
 
+  function addName() {
+    if (newCat.length > 2) {
+      const overallCats = [...cats, newCat];
+      setCats(overallCats);
+      setNewCat;
+    }
+  }
 
   return (
-    <div className="AgeTotaller">
-      <div className="cats"></div>
-      {cats.map((cat, index)=>(
-        <AgeCounter key={index} cat={cat} update={update}/>
-        
-      ))}
+    <div>
+      <input
+        type="text"
+        value={newCat}
+        onChange={(event) => setNewCat(event.target.value)}
+      />
+      <button onClick={addName}> new name please</button>
+
+      <div className="AgeTotaller">
+        <div className="cats">
+          {cats.map((cat, index) => (
+            <AgeCounter key={index} cat={cat} improved={improved} />
+          ))}
+          <h1>The Total Age is: {totalAge}</h1>
+        </div>
+      </div>
     </div>
   );
 };
